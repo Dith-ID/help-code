ed8e290977eecda76cc2a107d60a296674593129
Dith-ID <70526408+Dith-ID@users.noreply.github.com>
2020-09-02 07:44:10 +0700

    Add files via upload

---
+++ package.js
@@ -0,0 +1,44 @@
+{
+  "//1": "describes your app and its dependencies",
+  "//2": "https://docs.npmjs.com/files/package.json",
+  "//3": "updating this file will download and update your packages",
+  "name": "hello-express",
+  "version": "0.0.1",
+  "description": "A simple Node app built on Express, instantly up and running.",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.17.1",
+    "discord.js": "^12.3.1",
+    "ms": "^2.1.2",
+    "mathjs": "^7.2.0",
+    "imdb-api": "^4.2.0",
+    "moment": "^2.27.0",
+    "something-random-on-discord": "^0.0.1",
+    "quick.db": "^7.1.1",
+    "os": "^0.1.1",
+    "parse-ms": "^2.1.0",
+    "cpu-stat": "^2.0.1",
+    "moment-duration-format": "^2.3.2",
+    "weather-js": "^2.0.0",
+    "novelcovid": "^1.2.6",
+    "request-promise-native": "^1.0.9",
+    "anime-scraper": "^3.1.1",
+    "ascii-table": "^0.0.9",
+    "node-superfetch": "^0.1.10"
+  },
+  "engines": {
+    "node": "12.x"
+  },
+  "repository": {
+    "url": "https://glitch.com/edit/#!/hello-express"
+  },
+  "license": "MIT",
+  "keywords": [
+    "node",
+    "glitch",
+    "express"
+  ]
+}
